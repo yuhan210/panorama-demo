69f6e732e181ae5f1f1df136a9e587fc860454aa
root <root@ip-10-239-187-120.ec2.internal>
2016-11-06 23:17:44 +0000

    create feature/reverse index

--- pre-index/create-index.py
+++ pre-index/create-index.py
@@ -5,10 +5,11 @@ import math
 import pickle
 import inflection
 import redis
+import numpy as np
 from utils import load_all_modules_dict_local, word_pref_to_stopword_pref_dict
 from nlp import get_stopwords
 
-r = redis.StrictRedis()
+_redis = redis.StrictRedis()
 stop_words = get_stopwords(1)
 wptospd = word_pref_to_stopword_pref_dict()
 
@@ -86,23 +87,29 @@ def get_objtags(video_name, vgg_dict, msr_cap_dict, rcnn_bbx_dict, video_len, VG
 
     return msr, vgg, rcnn
 
-def get_features(msr, vgg, rcnn, start_fid, end_fid, width, height):
+def get_features(msr, vgg, rcnn, start_fid, end_fid, width, height, fps):
 
     # object tags
     obj_tags = normalize_objtags(msr, vgg, rcnn, start_fid, end_fid)
 
     # visual features
-    get_visual_features(rcnn, start_fid, end_fid, width, height)
+    obj_vis_features = get_visual_features(rcnn, start_fid, end_fid, width, height, fps)
 
-def get_visual_features(rcnn, start_fid, end_fid, width, height):
+    return obj_tags, obj_vis_features
+
+def get_visual_features(rcnn, start_fid, end_fid, width, height, fps):
     #rcnn_tfs += [{'score': score, 'bbox': bbox, 'obj': obj}]
-    rcnn_objtags = list(set([rcnn[x]['obj'] for x in xrange(start_fid, end_fid)]))
+    rcnn_objtags = []
+    for x in xrange(start_fid, end_fid):
+        for obj_info in rcnn[x]:
+            if obj_info['obj'] not in rcnn_objtags:
+                rcnn_objtags += [obj_info['obj']]
     obj_features = dict(zip(rcnn_objtags, [{} for x in rcnn_objtags]))
 
     for target_objtag in rcnn_objtags:
         sizes = [] 
         dwell_frames = []
-        moving_speed = []
+        moving_speeds = []
         prev_obj = None 
         for fid in xrange(start_fid, end_fid):  
             # dwell time, size (biggest obj), moving speed (biggest obj)
@@ -112,7 +119,6 @@ def get_visual_features(rcnn, start_fid, end_fid, width, height):
             max_obj_bbx = None
             for obj_idx, obj in enumerate(rcnn[fid]):
                 if obj['obj'] == target_objtag:
-                    dwell_frames += [fid]
 
                     bbox = obj['bbox']
                     bbox_w = (bbox[2] - bbox[0])/(width * 1.0)
@@ -122,16 +128,29 @@ def get_visual_features(rcnn, start_fid, end_fid, width, height):
                     if size > max_obj_size:    
                         max_obj_size = size
                         max_obj_bbx = bbox
+
             # the target obj tag is in this frame
             if max_obj_size > 0:
                 sizes += [max_obj_size]
+                dwell_frames += [fid]
 
                 if prev_obj == None: 
                     prev_obj = (fid, (max_obj_bbx[2] + max_obj_bbx[0])/2, (max_obj_bbx[3] + max_obj_bbx[1])/2)
                 else:
-                 
+                    dist = get_dist(prev_obj, max_obj_bbx, width, height) 
+                    time = (fid - prev_obj[0])/float(fps)
+                    speed = dist/time
+                    moving_speeds += [speed] 
                     
-        dwell_time = len(list(set(dwell_frames)))
+        dwell_time = len(dwell_frames)
+        ave_moving_speed = np.mean(moving_speeds)
+        ave_size = np.mean(sizes)
+
+        obj_features[target_objtag] = {'moving_speed': ave_moving_speed, 'dwell_time': dwell_time, 'size': ave_size}
+    return obj_features
+
+def get_dist(x, y, w, h):
+    return math.sqrt( ((x[1] - y[1])/(w * 1.0))** 2 + ((x[2]-y[2])/(h * 1.0)) ** 2)
 
 def normalize_objtags(msr, vgg, rcnn, start_fid, end_fid, OCCUR_PERC = 0.3):
 
@@ -154,31 +173,51 @@ def normalize_objtags(msr, vgg, rcnn, start_fid, end_fid, OCCUR_PERC = 0.3):
     return window_objs
 
 
-def store_feature_index(video_name, msr, vgg, rcnn, video_len):
-    WINDOW_STEP = 1 * int(round(video_frame_rate[video_name][0]))
-    WINDOW_SIZE = 5 * int(round(video_frame_rate[video_name][0]))
+def create_feature_index(vid, video_name, msr, vgg, rcnn, video_len):
+    fps = int(round(video_frame_rate[video_name][0]))
+    WINDOW_STEP = 1 * fps
+    WINDOW_SIZE = 5 * fps
     width = video_frame_rate[video_name][1]    
     height = video_frame_rate[video_name][2] 
- 
+
     end_fid = WINDOW_SIZE
+    ts = 5
     while end_fid < video_len:
         start_fid = end_fid - WINDOW_SIZE 
-        get_features(msr, vgg, rcnn, start_fid, end_fid, width, height)
+        print start_fid, end_fid
+        obj_tags, obj_vis_features = get_features(msr, vgg, rcnn, start_fid, end_fid, width, height, fps)
+        # feature table
+        redis_feature_value = {'obj_tags': obj_tags, 'obj_vis_features': obj_vis_features}   
+        redis_feature_key = str(ts) + ':' + str(vid)
+        _redis.set(redis_feature_key, pickle.dumps(redis_feature_value))
         
+        # reverse table
+        for tag in obj_tags:
+            redis_reverse_key = str(ts) + ':' + str(tag)
+            _redis.sadd(redis_reverse_key, vid)
+
         end_fid += WINDOW_STEP 
+        ts += 1
+
 
 if __name__ == "__main__":
+    '''
+    vid = [1,2]
+    key = "123-dog"
+    _redis.sadd(key, *vid)
+    p = _redis.smembers(key)
+    print p
+    vid = [2,3,5]
+    _redis.sadd(key, *vid)
+    p = _redis.smembers(key)
+    print p
+    '''
 
-    #vid = [1,2,3,4]
-    #key = "123-dog"
-    #r.sadd(key, *vid)
-    #p = r.smembers(key)
+    for vid, video_name in enumerate(video_names):
+        print vid, video_name
 
-    for video_name in video_names:
-        print video_name
         video_len = video_lengths[video_name]
         rcnn_dict, vgg_dict, dummy, msr_cap_dict, (rcnn_bbx_data, rcnn_bbx_dict) = load_all_modules_dict_local(video_name)
         msr, vgg, rcnn = get_objtags(video_name, vgg_dict, msr_cap_dict, rcnn_bbx_dict, video_len)
 
-        store_feature_index(video_name, msr, vgg, rcnn, video_len)
-
+        create_feature_index(vid, video_name, msr, vgg, rcnn, video_len)
